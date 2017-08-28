b141da17155aeca71dffada7cc309f57386a1842
Carmen <ccincotti3@gmail.com>
2017-08-28 13:45:56 -0400

    Add pause button functionality

--- src/stopwatch.jsx
+++ src/stopwatch.jsx
@@ -3,24 +3,30 @@ import React from 'react';
 class Stopwatch extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {startTime: Date.now(), currentTime: Date.now()}
+      this.state = {startTime: Date.now(), currentTime: Date.now(), pauseTime: 0, on: false}
       this.start = this.start.bind(this);
       this.reset = this.reset.bind(this);
+      this.pause = this.pause.bind(this);
     }
 
     start(){
-      this.setState({startTime: Date.now(), currentTime: Date.now()})
+      this.setState({startTime: Date.now(), currentTime: Date.now() + this.state.pauseTime, on: true})
       if(this.tickTock) {
         clearInterval(this.tickTock);
       }
       this.tickTock = setInterval(
-        () => this.setState({currentTime: Date.now()})
+        () => this.setState({currentTime: Date.now() + this.state.pauseTime})
       , 10);
     }
 
     reset() {
       clearInterval(this.tickTock);
-      this.setState({startTime: 0, currentTime: 0});
+      this.setState({startTime: 0, currentTime: 0, on: false});
+    }
+
+    pause() {
+      clearInterval(this.tickTock);
+      this.setState({pauseTime: this.state.currentTime - this.state.startTime, on: false})
     }
 
     parseTime(time) {
@@ -41,10 +47,12 @@ class Stopwatch extends React.Component {
 
     render() {
       const time = this.parseTime(this.state.currentTime - this.state.startTime)
+      const buttonText = this.state.on ? "Pause" : "Start"
+      const buttonFunc = this.state.on ? this.pause : this.start
       return (
         <div>
           <h1>{time}</h1>
-          <button onClick={this.start}>Start</button>
+          <button onClick={buttonFunc}>{buttonText}</button>
           <button onClick={this.reset}>Reset</button>
         </div>
       )
